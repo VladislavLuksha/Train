ddb71b772a7f90ebbd6a4d5c61cc435b4a1860b6
VladislavLuksha <49606310+VladislavLuksha@users.noreply.github.com>
2019-07-01 22:41:17 +0300

    The first version of root exstraction programm.

---
+++ RootNDegree/root n degree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace root_n_degree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculater number = new Calculater();
+            double resultNumber = number.RootExtraction(81,4,1E-10);
+            Console.WriteLine(resultNumber);
+        }
+    }
+}
---
+++ RootNDegree/root n degree/Radix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace root_n_degree
+{
+    class Calculater
+    {
+        public double RootExtraction(double number,int power,double accuracy)
+        {
+            double tempEarlyNumber=number/2;
+            double tempEarlyPower= Math.Pow(tempEarlyNumber, power);
+            double summNumber=0;
+            double tempSecondNumber = number;
+            double summPower = 0;
+            while (Math.Abs(number-summPower)>=accuracy)
+            { 
+                if (tempEarlyPower > number)
+                {
+                    tempSecondNumber = tempEarlyNumber;
+                    tempEarlyNumber = tempSecondNumber / 2;
+                    tempEarlyPower = Math.Pow(tempEarlyNumber, power);
+                    summNumber = (tempEarlyNumber + tempSecondNumber) / 2;
+                    summPower = Math.Pow(summNumber, power);
+                }
+                if (summPower > number)
+                {
+                    tempSecondNumber = summNumber;
+                    summNumber = (tempEarlyNumber + tempSecondNumber) / 2;
+                    summPower = Math.Pow(summNumber, power);
+                }
+                else
+                {
+                    tempEarlyNumber = summNumber;
+                    summNumber = (tempEarlyNumber + tempSecondNumber) / 2;
+                    summPower = Math.Pow(summNumber, power);
+                }
+            } 
+            return summNumber;
+        }
+    }
+}
