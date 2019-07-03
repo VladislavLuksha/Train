567d2602d215cf096452c8a350a6f6a3ecb6c099
VladislavLuksha <49606310+VladislavLuksha@users.noreply.github.com>
2019-07-03 19:26:34 +0300

    Added DynamicArray.

---
+++ DynamicArray/DynamicArray/DynamicArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    class DynamicArray
+    {
+        int[] data;
+        int capacity;
+        int length;
+        const int expandCoef=2;
+
+        public DynamicArray()
+        {
+            capacity = 16;
+            data = new int[capacity];
+        }
+        public DynamicArray(int size)
+        {
+            capacity = size;        
+            data = new int[capacity];
+        }
+
+        public void Add(int number)
+        {
+            if(data.Length==capacity)
+            {
+                int[] data1 = new int[expandCoef * capacity];
+                for (int i=0;i<=data.Length-1;i++)
+                {
+                    data1[i] = data[i];
+                }
+                data1[data.Length] = number;
+                data = data1;
+            }
+            else
+            {
+                data[data.Length] = number;
+            }
+        }
+    }
+}
---
+++ DynamicArray/DynamicArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray array1 = new DynamicArray(20);
+            DynamicArray array2 = new DynamicArray();
+            array2.Add(5);
+
+        }
+    }
+}
