55aadfe55667490acddb3f39534c923db49c340c
VladislavLuksha <49606310+VladislavLuksha@users.noreply.github.com>
2019-07-04 15:55:35 +0300

    Realization Stack.

---
+++ StackInt/Stack(int)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_int_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(5);
+            Console.WriteLine(stack.isEmpty());
+            Console.WriteLine(stack.isFull());
+            stack.Push(5);
+            stack.Push(10);
+            stack.Push(15);
+            stack.Push(3);
+            stack.Push(7);
+            Console.WriteLine(stack.isFull());
+            stack.PrintArray();
+            Console.WriteLine(stack.Count());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Count());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Count());
+             
+        }
+    }
+}
---
+++ StackInt/Stack(int)/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_int_
+{
+    class Stack
+    {
+        int[] array;
+        int size;
+        int top;
+
+        public Stack(int size)
+        {
+            this.size = size;
+            array = new int[size];
+            top = 0;
+        }
+
+        public bool isEmpty()
+        {
+            return top == 0;
+        }
+        public bool isFull()
+        {
+            return top == size;
+        }
+
+        public int Count()
+        {
+            return top;
+        }
+        public void Push(int element)
+        {
+            array[top] = element;
+            top++;
+        }
+        public int Peek()
+        {
+            return array[top-1];
+        }
+        public int Pop()
+        {
+            return array[--top];
+        }
+        public void PrintArray()
+        {
+            for(int i=top-1;i>=0;i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
