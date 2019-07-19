9f10b74f3fef52c764741355161198c7c438d9a0
VladislavLuksha <49606310+VladislavLuksha@users.noreply.github.com>
2019-07-19 11:22:19 +0300

    Validator implementation

---
+++ Validator/Validator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validation phoneNumber = new Validation();
+            phoneNumber.PhoneNumberCheck("375204219487");
+        }
+    }
+}
---
+++ Validator/Validator/Validation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator
+{
+    class Validation
+    {
+        const string prefixMTC = "+37533";
+        const string prefixVEL = "+37529";
+        const string prefixLIFE = "+37525";
+        const string prefixVelcome = "+37544";
+
+        public void PhoneNumberCheck(string line)
+        {
+            bool check = false;
+            int counterNumber = 0;
+            for (int i = 0; i < prefixMTC.Length; i++)
+            {
+                if (prefixMTC[i] == line[i] || prefixLIFE[i] == line[i] || prefixVEL[i] == line[i] || prefixVelcome[i] == line[i])
+                    check = true;
+                else
+                    check = false;
+                if (check == false)
+                    break;
+            }
+            for (int i = 1; i < line.Length; i++)
+            {   
+                if (Char.IsDigit(line[i]))
+                    counterNumber++;
+                else
+                    break;
+            }
+            if (check == true && counterNumber == 12)
+            {
+                Console.WriteLine("Phone number validary!");
+            }
+            else
+                Console.WriteLine("Phone number not validary!");
+        }
+    }
+}
