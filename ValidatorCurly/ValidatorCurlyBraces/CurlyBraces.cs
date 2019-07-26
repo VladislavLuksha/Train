0937ee1b7fc4e126a82ed5534625b5d295f72ece
VladislavLuksha <49606310+VladislavLuksha@users.noreply.github.com>
2019-07-26 14:40:11 +0300

    Correction of defects.

---
+++ ValidatorCurly/ValidatorCurlyBraces/CurlyBraces.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorCurlyBraces
+{
+    class CurlyBraces
+    {
+        Stack<char> StackSymbols=new Stack<char>();
+        Dictionary<char, char> DictionarySymbols = new Dictionary<char, char> { { ')', '(' }, { ']', '[' }, { '}', '{' } };
+
+        public bool Check (string line)
+        {
+            bool checkSymbols=true;
+            for(int i=0;i<line.Length;i++)
+            {
+                if(DictionarySymbols.ContainsValue(line[i]))
+                {
+                    StackSymbols.Push(line[i]);
+                    checkSymbols = true;
+                }
+                /*
+                foreach(char element in DictionarySymbols.Values)
+                {
+                    if (line[i] == element)
+                    {
+                        StackSymbols.Push(element);
+                        checkSymbols = true;
+                        break;
+                    }
+                }
+                */
+                if (StackSymbols.Count != 0)
+                {
+                    if(DictionarySymbols.ContainsKey(line[i]))
+                    {
+                        if (DictionarySymbols[line[i]] == StackSymbols.Peek())
+                        {
+                            StackSymbols.Pop();
+                        }
+                        else
+                        {
+                            checkSymbols = false;
+                            break;
+                        }
+                    }
+                    /*
+                    foreach (char element in DictionarySymbols.Keys)
+                    {
+                        if (element == line[i])
+                        {
+                            if (DictionarySymbols[element] == StackSymbols.Peek())
+                            {
+                                StackSymbols.Pop();
+                            }
+                            else
+                            {
+                                checkSymbols = false;
+                            }
+                        }
+                    }
+                    */
+                }
+                else
+                {
+                    checkSymbols = false;
+                }
+            }
+            /*
+            if(StackSymbols.Count!=0)
+            {
+                checkSymbols = false;
+            }
+            */
+            if (checkSymbols == true)
+                return true;
+            else
+                return false;
+        }
+            
+    }
+}
---
+++ ValidatorCurly/ValidatorCurlyBraces/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorCurlyBraces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CurlyBraces line = new CurlyBraces();
+            bool ResultCheck = line.Check("([(])");
+            if(ResultCheck==true)
+            {
+                Console.WriteLine("Validation!");
+            }
+            else
+            {
+                Console.WriteLine("No Validation!");
+            }
+        }
+    }
+}
