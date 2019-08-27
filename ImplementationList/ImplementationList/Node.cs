be55964790a39b08171252b57b63e77ab3ba69fd
Vladislav <vladislavluksha99@gmail.com>
2019-08-27 13:12:29 +0300

    List implementation.

---
+++ ImplementationList/ImplementationList/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ImplementationList
+{
+    class Node
+    {
+        public object value;
+        public Node nextNode { get; private set; }
+        public Node(object value)
+        {
+            nextNode = null;
+            this.value = value;
+        }
+
+        public void SetNextNode(Node node)
+        {
+            nextNode = node;
+        }
+
+        public void PrintNode(Node node)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
